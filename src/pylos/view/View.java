f7727cd19f95ac44596516d5247587bab30d253c
nico <nicolas.f.gerard@gmail.com>
2011-02-18 14:34:37 +0100

    Draw a basic board and configure the camera.

--- src/pylos/view/View.java
+++ src/pylos/view/View.java
@@ -1,6 +1,15 @@
 package pylos.view;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.input.ChaseCamera;
+import com.jme3.light.DirectionalLight;
+import com.jme3.light.PointLight;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
 import com.jme3.system.AppSettings;
 
 public class View extends SimpleApplication {
@@ -14,9 +23,30 @@ public class View extends SimpleApplication {
 
 	@Override
 	public void simpleInitApp() {
+		Geometry board = makeBoard();
+		rootNode.attachChild(board);
 
+		flyCam.setEnabled(false);
+		ChaseCamera chaseCam = new ChaseCamera(cam, board, inputManager);
+		chaseCam.setInvertVerticalAxis(true);
+		
+		/*
+		 * DirectionalLight sun = new DirectionalLight();
+		sun.setDirection(new Vector3f(0, -1, 0));
+		sun.setColor(ColorRGBA.White);
+		rootNode.addLight(sun);
+		*/
 	}
-
+	
+	public Geometry	makeBoard() {
+		Box board = new Box(new Vector3f(), 2, 0.1f, 2);
+		Geometry geom = new Geometry("board", board);
+		Material mat = new Material(assetManager, "Common/MatDefs/Misc/SolidColor.j3md");
+		mat.setColor("Color", ColorRGBA.Gray);
+		geom.setMaterial(mat);
+		return geom;
+	}
+	
 	public void show() {
 		start();
 	}
