00e213dc9ab399ddc274de16b501c3255b15920d
eregon <eregontp@gmail.com>
2011-03-08 11:48:53 +0100

    add int coordinates for the Ball model

--- src/pylos/model/Ball.java
+++ src/pylos/model/Ball.java
@@ -5,6 +5,7 @@ import pylos.view.BallGraphics;
 public class Ball {
 	public final Player owner;
 	public boolean onBoard = false;
+	public int x, y, z;
 	public BallGraphics graphics;
 
 	public Ball(Player owner) {
