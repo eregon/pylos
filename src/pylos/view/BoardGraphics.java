c6751f840c2b1f5210a8d4199b9de1eff54a9589
eregon <eregontp@gmail.com>
2011-03-11 16:57:43 +0100

    align balls in height

--- src/pylos/view/BoardGraphics.java
+++ src/pylos/view/BoardGraphics.java
@@ -11,7 +11,7 @@ import com.jme3.scene.Spatial;
 public class BoardGraphics {
 	static final float HORIZONTAL_SCALE = 5;
 	static final float VERTICAL_SCALE = 0.5f;
-	static final float BOARD_HEIGHT = 0.9f;
+	static final float BOARD_HEIGHT = 1f;
 	static final int BALLS_BY_SIDE = 9;
 	static final int BALLS_BY_SMALL_SIDE = 3;
 	Spatial board;
