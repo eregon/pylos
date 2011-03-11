2925177af2980b8eb958a67d689304d29f9e48e2
nico <nicolas.f.gerard@gmail.com>
2011-03-11 14:57:36 +0100

    Set new material for balls

--- src/pylos/view/BallGraphics.java
+++ src/pylos/view/BallGraphics.java
@@ -2,6 +2,7 @@ package pylos.view;
 
 import pylos.Pylos;
 import pylos.model.Ball;
+import pylos.model.Model;
 
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
@@ -29,10 +30,15 @@ public class BallGraphics {
 	}
 
 	public void create() {
+		Material mat;
 		Sphere s = new Sphere(50, 50, DIAMETER / 2);
 		geometry = new Geometry("Ball #" + model.i + " (" + model.owner.side + ")", s);
-		Material mat = new Material(Pylos.view.getAssetManager(), "Common/MatDefs/Misc/SolidColor.j3md");
-		mat.setColor("Color", ColorRGBA.randomColor());
+		if (model.owner == Model.player1) {
+			mat = new Material(Pylos.view.getAssetManager(), "Common/MatDefs/Light/Lighting.j3md"); 
+		} else {
+			mat = new Material(Pylos.view.getAssetManager(), "Common/MatDefs/Light/Lighting.j3md");
+			mat.setFloat("Shininess", 0.5f);
+		}
 		geometry.setMaterial(mat);
 	}
 }
