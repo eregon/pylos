d7f9a882798ebfa2b5836af797326e5303edfb78
eregon <eregontp@gmail.com>
2011-03-11 15:00:18 +0100

    cleanup

--- src/pylos/view/BallGraphics.java
+++ src/pylos/view/BallGraphics.java
@@ -5,7 +5,6 @@ import pylos.model.Ball;
 import pylos.model.Model;
 
 import com.jme3.material.Material;
-import com.jme3.math.ColorRGBA;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Sphere;
 
@@ -34,7 +33,7 @@ public class BallGraphics {
 		Sphere s = new Sphere(50, 50, DIAMETER / 2);
 		geometry = new Geometry("Ball #" + model.i + " (" + model.owner.side + ")", s);
 		if (model.owner == Model.player1) {
-			mat = new Material(Pylos.view.getAssetManager(), "Common/MatDefs/Light/Lighting.j3md"); 
+			mat = new Material(Pylos.view.getAssetManager(), "Common/MatDefs/Light/Lighting.j3md");
 		} else {
 			mat = new Material(Pylos.view.getAssetManager(), "Common/MatDefs/Light/Lighting.j3md");
 			mat.setFloat("Shininess", 0.5f);
