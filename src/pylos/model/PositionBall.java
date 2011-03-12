64e7bffa111cc79824841fa1d2b7d46b5c754059
eregon <eregontp@gmail.com>
2011-03-12 21:24:43 +0100

    constructor should be first

--- src/pylos/model/PositionBall.java
+++ src/pylos/model/PositionBall.java
@@ -3,14 +3,14 @@ package pylos.model;
 public class PositionBall {
 	public int x, y, z;
 
-	@Override
-	public String toString() {
-		return "(" + x + ", " + y + ", " + z + ")";
-	}
-
 	public PositionBall(int x, int y, int z) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
+	}
+
+	@Override
+	public String toString() {
+		return "(" + x + ", " + y + ", " + z + ")";
 	}
 }
