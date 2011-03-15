73440949e21e303deca889dfb3401af913f8c356
eregon <eregontp@gmail.com>
2011-03-15 18:10:26 +0100

    add a Logger for Pylos

--- src/pylos/Pylos.java
+++ src/pylos/Pylos.java
@@ -1,6 +1,7 @@
 package pylos;
 
 import java.io.File;
+import java.util.logging.Logger;
 
 import pylos.controller.Controller;
 import pylos.model.Model;
@@ -11,7 +12,8 @@ public class Pylos {
 	public static View view;
 	public static Controller controller;
 
-	public static File rootPath = new File(".");
+	public static final File rootPath = new File(".");
+	public static final Logger logger = Logger.getLogger(Pylos.class.getName());
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Pylos!");
