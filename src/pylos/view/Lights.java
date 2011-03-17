dc14a603e5a686f69df7bc4b5cba963edfa48c5e
eregon <eregontp@gmail.com>
2011-03-17 16:36:23 +0100

    extract constants

--- src/pylos/view/Lights.java
+++ src/pylos/view/Lights.java
@@ -2,21 +2,26 @@ package pylos.view;
 
 import com.jme3.light.PointLight;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
 import com.jme3.math.Vector3f;
 
 public class Lights {
 	final static int radius = 20;
+	final static int lights = 4;
+	final static float distance = 11.5f;
+	final static float height = 4;
 
 	public Lights(View view) {
 		PointLight light;
-		for (int x = 0; x < 2; x++) {
-			for (int y = 0; y < 2; y++) {
-				light = new PointLight();
-				light.setColor(ColorRGBA.White);
-				light.setRadius(radius);
-				light.setPosition(new Vector3f((x == 0 ? 8 : -8), 4, (y == 0 ? 8 : -8)));
-				view.getRootNode().addLight(light);
-			}
+		float angle = FastMath.PI / lights;
+
+		for (int i = 0; i < lights; i++) {
+			angle += 2 * FastMath.PI / lights;
+			light = new PointLight();
+			light.setColor(ColorRGBA.White);
+			light.setRadius(radius);
+			light.setPosition(new Vector3f(distance * FastMath.cos(angle), height, distance * FastMath.sin(angle)));
+			view.getRootNode().addLight(light);
 		}
 	}
 }
