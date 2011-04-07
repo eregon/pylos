6c87bda70ac459f8d6951c072e5f3cd90de48ecc
eregon <eregontp@gmail.com>
2011-04-07 22:46:04 +0200

    complete setup.sh with jUnit, and add a few notes in the ReadMe

--- setup.sh
+++ setup.sh
@@ -4,9 +4,10 @@ if [ ! -e lib ]; then
 fi
 cd lib
 
+# jMonkeyEngine 3
 if [ ! -e jME.zip -a ! -e jME ]; then
   echo "Downloading jME ..."
-  curl http://eregon.me/pylos/jME.zip -o jME.zip
+  curl http://eregon.me/pylos/jME-light.zip -o jME.zip
 fi
 
 if [ ! -e jME ]; then
@@ -17,4 +18,13 @@ cd jME
 if [ ! -e jMonkeyEngine3.jar ]; then
   echo "Unpacking jME.zip"
   unzip -q ../jME.zip
+  rm ../jME.zip
+fi
+
+# jUnit
+cd ..
+
+if [ ! -e junit-4.8.2.jar ]; then
+  echo "Downloading jUnit ..."
+  curl https://github.com/downloads/KentBeck/junit/junit-4.8.2.jar -o junit-4.8.2.jar
 fi
