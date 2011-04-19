f52a55a3a763fa03710ecf430ab34439f2730df3
nico <nicolas.f.gerard@gmail.com>
2011-04-19 09:42:54 +0200

    eraze error param from gameSample() in mainTest

--- src/pylos/test/MainTest.java
+++ src/pylos/test/MainTest.java
@@ -23,7 +23,6 @@ public class MainTest extends PylosTestCase {
 	}
 
 	public void testGetPositionBalls() {
-
 	}
 
 	public void testBallAt() {
@@ -34,12 +33,12 @@ public class MainTest extends PylosTestCase {
 		assertTrue(Model.canPlaceBallAt(Position.at(0, 0, 0)));
 		assertFalse(Model.canPlaceBallAt(Position.at(0, 0, 1))); // plateau vide donc pas de sens placer boule z = 1
 
-		gameSample(false);
+		gameSample();
 		assertFalse(Model.canPlaceBallAt(Position.at(0, 0, 0)));
 		assertFalse(Model.canPlaceBallAt(Position.at(0, 0, 1)));
 		assertFalse(Model.canPlaceBallAt(Position.at(1, 0, 0)));
 		assertTrue(Model.canPlaceBallAt(Position.at(1, 2, 0)));
-		assertTrue(Model.canPlaceBallAt(Position.at(1, 0, 1)));
+		assertFalse(Model.canPlaceBallAt(Position.at(1, 0, 1)));
 	}
 
 	public void testAccessibleBalls() {
@@ -75,32 +74,24 @@ public class MainTest extends PylosTestCase {
 		assertFalse(Model.board.anyBallAt(Position.at(0, 0, 0)));
 		assertFalse(Model.board.anyBallAt(Position.at(1, 2, 2)));
 
-		gameSample(false);
+		gameSample();
 		ball.placeAt(Position.at(2, 2, 0));
 		assertFalse(Model.board.anyBallAt(Position.at(2, 2, 0)));
 	}
 
 	/**
-	 * The parametre error is to create a game were there is an error like one ball at z = 1 and no balls under.
-	 * No matter, i'll delete this if no need ...
-	 * 
-	 * @param error
+	 * xxxx
+	 * xx0x	xxx
+	 * 00xx	xxx
+	 * 000x	0xx
 	 */
-	public void gameSample(boolean error) {
-		if (error) {
-			Model.currentPlayer.putBallOnBoard(Position.at(0, 0, 0));
-			Model.currentPlayer.putBallOnBoard(Position.at(1, 1, 1));
-			Model.currentPlayer.putBallOnBoard(Position.at(1, 2, 1));
-			Model.currentPlayer.putBallOnBoard(Position.at(0, 1, 1));
-			Model.currentPlayer.putBallOnBoard(Position.at(0, 0, 1));
-		} else {
-			Model.currentPlayer.putBallOnBoard(Position.at(0, 0, 0));
-			Model.currentPlayer.putBallOnBoard(Position.at(0, 1, 0));
-			Model.currentPlayer.putBallOnBoard(Position.at(1, 1, 0));
-			Model.currentPlayer.putBallOnBoard(Position.at(1, 0, 0));
-			Model.currentPlayer.putBallOnBoard(Position.at(0, 0, 1));
-			Model.currentPlayer.putBallOnBoard(Position.at(2, 0, 0));
-			Model.currentPlayer.putBallOnBoard(Position.at(2, 1, 0));
-		}
+	public void gameSample() {
+		Model.currentPlayer.putBallOnBoard(Position.at(0, 0, 0));
+		Model.currentPlayer.putBallOnBoard(Position.at(0, 1, 0));
+		Model.currentPlayer.putBallOnBoard(Position.at(1, 1, 0));
+		Model.currentPlayer.putBallOnBoard(Position.at(1, 0, 0));
+		Model.currentPlayer.putBallOnBoard(Position.at(0, 0, 1));
+		Model.currentPlayer.putBallOnBoard(Position.at(2, 0, 0));
+		Model.currentPlayer.putBallOnBoard(Position.at(2, 2, 0));
 	}
 }
