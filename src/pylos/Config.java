ac8d736499e120a0589363ea985efa29b5689731
eregon <eregontp@gmail.com>
2011-05-03 11:34:51 +0200

    log everything in files, one by Logger

--- src/pylos/Config.java
+++ src/pylos/Config.java
@@ -1,13 +1,36 @@
 package pylos;
 
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class Config {
 	public static boolean LOW_GRAPHICS = false; // Set this to true if it is too slow
 
 	public static void configureProject() {
-		Logger.getLogger("").setLevel(Level.WARNING); // Hide everything under Warning by default
-		Logger.getLogger("pylos").setLevel(Level.CONFIG);
+		configureLogger();
+	}
+
+	public static void configureLogger() {
+		// Hide everything under Warning for default handlers
+		for (Handler handler : Logger.getLogger("").getHandlers()) {
+			handler.setLevel(Level.WARNING);
+		}
+
+		createFileLogger(Pylos.logger);
+		createFileLogger(Logger.getLogger("com.jme3"));
+		createFileLogger(Logger.getLogger("de.lessvoid.nifty"));
+	}
+
+	public static void createFileLogger(Logger logger) {
+		try {
+			FileHandler fh = new FileHandler("log/" + logger.getName() + ".log");
+			fh.setFormatter(new SimpleFormatter());
+			logger.addHandler(fh);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
