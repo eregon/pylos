99e6b245158d168796557607e5b7c47ae4cb68bf
eregon <eregontp@gmail.com>
2011-05-07 16:49:49 +0200

    use Iterator instead of deprecated Enumeration

--- src/pylos/ai/GameState.java
+++ src/pylos/ai/GameState.java
@@ -1,6 +1,6 @@
 package pylos.ai;
 
-import java.util.Enumeration;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -8,44 +8,43 @@ import pylos.model.Ball;
 import pylos.model.Model;
 import pylos.model.Position;
 
-@SuppressWarnings("rawtypes")
-public class GameState implements Enumeration {
-	
+public class GameState implements Iterable<GameState> {
+
 	List<Ball> balls;
 	List<Ball> onBoard;
 	List<GameState> possibleMoves;
 	List<Move> moves;
 	private int score;
-	
+
 	public GameState(List<Ball> balls) {
 		moves = new LinkedList<Move>();
 		for (Ball ball : balls) {
-			if(ball.onBoard)
+			if (ball.onBoard)
 				onBoard.add(ball);
 			else
 				this.balls.add(ball);
 		}
 	}
-	
+
 	public GameState(List<Ball> onBoard, List<Ball> onSide) {
 		balls = onSide;
 		this.onBoard = onBoard;
 	}
-	
+
 	public int getScore() {
 		return score;
 	}
-	
+
 	public void add(int a) {
 		score += a;
 	}
-	
+
 	public void generatePosibleMove() {
 		for (Ball ball : onBoard) {
-			if(ball.isMountableByCurrentPlayer()) {
+			if (ball.isMountableByCurrentPlayer()) {
 				for (Position pos : Model.getPositionsToMount(ball)) {
 					Move move = new Move(ball, pos);
-					if(move.anyLineOrSquareForMove())
+					if (move.anyLineOrSquareForMove())
 						addRemovableStep(move);
 					moves.add(move);
 				}
@@ -58,35 +57,33 @@ public class GameState implements Enumeration {
 
 	private void addRemovableStep(Move move) {
 		for (Ball ball : onBoard) {
-			if(ball.isRemovableByCurrentPlayer())
+			if (ball.isRemovableByCurrentPlayer())
 				move.removables.add(new Move(ball, "remove"));
 			for (Ball ball2 : onBoard) {
-				if(ball2 != ball && ball2.isRemovableByCurrentPlayer() && ball.isRemovableByCurrentPlayer())
+				if (ball2 != ball && ball2.isRemovableByCurrentPlayer() && ball.isRemovableByCurrentPlayer())
 					move.removables.add(new Move(ball, ball2));
 			}
 		}
-		
+
 	}
 
-	@Override
-	public boolean hasMoreElements() {
-		return !moves.isEmpty();
-	}
+	public Iterator<GameState> iterator() {
+		List<GameState> list = new LinkedList<GameState>();
+		while (!moves.isEmpty()) {
+			List<Ball> oS = balls;
+			List<Ball> oB = onBoard;
+			Move move = moves.get(0);
+			if (move.removeStep) {
 
-	@Override
-	public GameState nextElement() {
-		List<Ball> oS = balls;
-		List<Ball> oB = onBoard;
-		Move move = moves.get(0);
-		if(move.removeStep) {
-			
+			}
+			moves.remove(0);
+			move.ball.position = move.position;
+			if (!move.ball.onBoard) {
+				oS.remove(move.ball);
+				oB.add(move.ball);
+			}
+			list.add(new GameState(oB, oS));
 		}
-		moves.remove(0);
-		move.ball.position = move.position;
-		if(!move.ball.onBoard) {
-			oS.remove(move.ball);
-			oB.add(move.ball);
-		}
-		return new GameState(oB, oS);
+		return list.iterator();
 	}
 }
